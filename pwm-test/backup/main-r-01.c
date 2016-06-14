94445c565191f09785a5299cbcead2629c983ed9
mbensalem <bensalem.messaoud@gmail.com>
2016-06-14 16:21:49 -0400

    ajout 2 fichiers dans backup

--- pwm-test/backup/main-r-01.c
+++ pwm-test/backup/main-r-01.c
@@ -32,8 +32,7 @@
   */
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
-#include <stdbool.h> 
-
+#include <stdbool.h>
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
@@ -48,211 +47,124 @@ TIM_HandleTypeDef htim2;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-void miscThread(void); 
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
                 
 
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
 
+/* USER CODE END PFP */
 
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+                                                                                                          
+ void m1dir(bool val) { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }    
+ void m2dir(bool val) { HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }    
+ void m3dir(bool val) { HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }    
 int main(void)
 {
-HAL_Init(); 
-SystemClock_Config();
-HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4); 
-miscThread(); 
 
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+
+
+   /*Configure GPIO pins : PC13 PC14 PC15 PC4                               
+                             PC5 */                                          
+GPIO_InitTypeDef GPIO_InitStruct;
+
+GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;                                     
+GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;                              
+GPIO_InitStruct.Pull = GPIO_NOPULL;                                      
+GPIO_InitStruct.Speed =GPIO_SPEED_FREQ_LOW;                             
+HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);                                  
+
+    /* PC13..15 = PWM enables */                                
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);        
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, GPIO_PIN_SET);        
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_SET);        
+
+m1dir(true); 
+
+/* USER CODE BEGIN 2 */
+   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1 | TIM_CHANNEL_2 | TIM_CHANNEL_3);
+	 
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);  // M1DIR set to 1
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_SET);  // M2DIR set to 1 
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_SET);  // M3DIR set to 1 
+	
+	 __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 180);
+	 __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 120);
+	 __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_3, 120);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
+  /* USER CODE END WHILE */
 
+  /* USER CODE BEGIN 3 */
 
   }
-
+  /* USER CODE END 3 */
 
 }
 
-
-static void Error_Handler(){
-
- printf("Error handler \r\n"); 
- while(1) ; 
-}
 /** System Clock Configuration
 */
 void SystemClock_Config(void)
 {
-	
-        RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_OscInitTypeDef RCC_OscInitStruct;
 
-	/* Enable Power Control clock */
-	__HAL_RCC_PWR_CLK_ENABLE();
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-	/*
-	 * The voltage scaling allows optimizing the power consumption when the device is
-	 * clocked below the maximum system frequency, to update the voltage scaling value
-	 * regarding system frequency refer to product datasheet.
-	 */
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  __HAL_RCC_PWR_CLK_ENABLE();
 
-	/* Enable HSE Oscillator and activate PLL with HSE as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 8;
-	RCC_OscInitStruct.PLL.PLLN = 336;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 7;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
-	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-		Error_Handler();
-	}
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-	/* STM32F405x/407x/415x/417x Revision Z devices: prefetch is supported  */
-	if (HAL_GetREVID() == 0x1001) {
-		/* Enable the Flash prefetch */
-		__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-	}
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
-
-
-
-
-void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
-{
-	if (htim->Instance == TIM2) {
-		GPIO_InitTypeDef gpio;
-
-		__HAL_RCC_TIM2_CLK_ENABLE();
-		__HAL_RCC_GPIOA_CLK_ENABLE();
-
-		/*
-		 * TIM2 GPIO Configuration
-		 * PA0 - TIM2_CH1
-		 * PA1 - TIM2_CH2
-		 * PA2 - TIM2_CH3
-		 */
-		memset(&gpio, 0, sizeof(gpio));
-		gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
-		gpio.Mode = GPIO_MODE_OUTPUT_PP;
-		gpio.Pull = GPIO_NOPULL;
-		gpio.Speed = GPIO_SPEED_HIGH;
-		gpio.Alternate = GPIO_AF1_TIM2;
-		HAL_GPIO_Init(GPIOA, &gpio);
-
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_RESET);
-	}
-}
-
-
-
-void gpio_init(void)
-{
-	GPIO_InitTypeDef gpio;
-
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-
-	memset(&gpio, 0, sizeof(gpio));
-
-	/* PA3 = M1DIR */
-	gpio.Pin = GPIO_PIN_3;
-	gpio.Mode = GPIO_MODE_OUTPUT_PP;
-	gpio.Pull = GPIO_NOPULL;
-	gpio.Speed = GPIO_SPEED_LOW;
-	HAL_GPIO_Init(GPIOA, &gpio);
-
-	/* PC13..15 = PWM enables */
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_SET);
-
-	/* PC4..5 = M2/M3DIR, PC13..15 = PWM enables */
-	gpio.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-	HAL_GPIO_Init(GPIOC, &gpio);
-
-	/* PD10..11 == LED1/2 */
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_10, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET);
-
-	gpio.Pin = GPIO_PIN_10 | GPIO_PIN_11;
-	HAL_GPIO_Init(GPIOD, &gpio);
-}
-
-
-
-void pwm_set(TIM_HandleTypeDef *htim, uint32_t which, uint16_t val)
-{
-	TIM_OC_InitTypeDef oc;
-
-	memset(&oc, 0, sizeof(oc));
-
-	oc.OCMode = TIM_OCMODE_PWM1;
-	oc.OCPolarity = TIM_OCPOLARITY_HIGH;
-	oc.OCFastMode = TIM_OCFAST_DISABLE;
-
-	oc.Pulse = val;
-
-	HAL_TIM_PWM_ConfigChannel(htim, &oc, which);
-}
-
-
-
-void pwm_init(TIM_HandleTypeDef *htim)
-{
-	TIM_ClockConfigTypeDef tclkcfg;
-	TIM_MasterConfigTypeDef tmcfg;
-
-	memset(htim, 0, sizeof(*htim));
-
-	htim->Instance = TIM2;
-	htim->Init.Prescaler = 24;
-	htim->Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim->Init.Period = 2000;
-	htim->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	HAL_TIM_PWM_Init(htim);
-
-#if 1
-	memset(&tclkcfg, 0, sizeof(tclkcfg));
-	memset(&tmcfg, 0, sizeof(tmcfg));
-
-	tclkcfg.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-	HAL_TIM_ConfigClockSource(htim, &tclkcfg);
-
-	tmcfg.MasterOutputTrigger = TIM_TRGO_RESET;
-	tmcfg.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-	HAL_TIMEx_MasterConfigSynchronization(htim, &tmcfg);
-#endif
-
-	HAL_TIM_PWM_Start(htim, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(htim, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Start(htim, TIM_CHANNEL_3);
-
-	pwm_set(htim, TIM_CHANNEL_1, 1500);
-	pwm_set(htim, TIM_CHANNEL_2, 1500);
-	pwm_set(htim, TIM_CHANNEL_3, 0);
-}
-
-
-
-void m1dir(bool val) { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }
-void m2dir(bool val) { HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }
-void m3dir(bool val) { HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, (val) ? GPIO_PIN_SET : GPIO_PIN_RESET); }
-
-
 
 /* TIM2 init function */
 void MX_TIM2_Init(void)
@@ -291,59 +203,6 @@ void MX_TIM2_Init(void)
   HAL_TIM_MspPostInit(&htim2);
 
 }
-
-
-void miscThread(void )
-{
-	TIM_HandleTypeDef htim;
-	int x;
-
-// 	(void)argument;
-
-	//usb_init();
-	gpio_init();
-	pwm_init(&htim);
-
-	m1dir(false);
-	m2dir(false);
-	m3dir(true);
-
-	x = 0;
-	while (1) {
-		bool toggle;
-		HAL_Delay(10); 
-
-		pwm_set(&htim, TIM_CHANNEL_1, x);
-		pwm_set(&htim, TIM_CHANNEL_2, x);
-		pwm_set(&htim, TIM_CHANNEL_3, x);
-
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
-//      	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
-//		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
-
-		++x;
-		if (x >= 2000) {
-			x = 0;
-		}
-
-
-		/* blinky */
-		if ((x % 10) == 0) {
-			if (toggle) {
-				toggle = false;
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_10, GPIO_PIN_RESET);
-			} else {
-				toggle = true;
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_10, GPIO_PIN_SET);
-			}
-		}
-
-		if ((x % 250) == 0) {
-			printf("Foo (%d)\n", 10);
-		}
-	};
-}
-
 
 /** Configure pins as 
         * Analog 
@@ -388,28 +247,12 @@ void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_4 
-                          |GPIO_PIN_5, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PC13 PC14 PC15 PC4 
-                           PC5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_4 
-                          |GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC2 PC3 */
   GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
@@ -433,6 +276,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC4 PC5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
@@ -537,6 +387,22 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+ /*Configure GPIO pins : PC13 PC14 PC15 PC4                               
+                           PC5 */                                          
+    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_4|GPIO_PIN_5;                                     
+   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;                              
+   GPIO_InitStruct.Pull = GPIO_NOPULL;                                      
+   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;                             
+   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+							 
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
 
 }
 
